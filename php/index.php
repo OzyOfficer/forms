18c54ebca6386bfdb03fee052b9af80be7d10625
OzyOfficer <pasha12_00@icloud.com>
2020-12-11 19:36:21 +0200

    New

--- php/index.php
+++ php/index.php
@@ -1,4 +1,8 @@
 <?php //error_reporting(0);
-$name = $_POST["imy"];
-mail("pasha12_00@icloud.com", "Input name", $name, "Content-type: text/html; charset=utf-8 \r\n");
-
+    $name = $_POST["imy"];
+    $email = $_POST["maill"];
+    $city = $_POST["city"];
+    $pidpus = "Збройні Сили України Test";
+    $forma1 ="Ім'я = ".$name."<br>"."Мейл = ".$email."<br>"."Город = ".$city;
+mail("darkopasha@gmail.com", "$pidpus", $forma1, "Content-type: text/html; charset=utf-8 \r\n");
+echo($forma1);
